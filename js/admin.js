cfe76df039cb5b9b67addadb6ab10445488c70a9
choilo11121999 <57838904+choilo11121999@users.noreply.github.com>
2019-12-02 17:12:34 +0700

    12/2/2019 3

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,12 @@
-function myFunction() {
+function hienNguoiDung() {
     document.getElementById("btnguoidung").onclick = function() {
-        document.getElementById("nguoidung").style.display = 'none';
+        document.getElementById("nguoidung").style.display = 'block';
+        document.getElementById("baidang").style.display = 'none'
+    };
+}
+function hienBaiDang() {
+    document.getElementById("btbaidang").onclick = function() {
+        document.getElementById("baidang").style.display = 'block';
+        document.getElementById("nguoidung").style.display = 'none'
     };
 }
